996901398c2ada8b7555963a39f4e0aa6c46adb5
Kh.Eliash <eliashebrahim@gmail.com>
2024-03-26 01:53:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Banner from "./Components/Banner/Banner";
+import Nav from "./Components/Nav/Nav";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto ">
+     
+         <Banner></Banner>
+       </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+ 
+
+const Banner = () => {
+    return (
+        <div className="bg-gray-100">
+            <div>
+            <h1>Books to freshen up your bookshelf</h1>
+            </div>
+            <div>
+                <img src="../../assets/Images/Cover.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
